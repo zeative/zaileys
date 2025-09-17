62b7c3c2c5db217c2230bc9dedf467a009b4ff82
zeative <zaadevofc@gmail.com>
2025-09-17 19:46:54 +0700

    add: more relay features

--- examples/test.ts
+++ examples/test.ts
@@ -3,20 +3,10 @@ import { Client } from "../src";
 const wa = new Client({
   authType: "pairing",
   phoneNumber: 6287833764462,
-  citation: {
-    my: [6285136635787, 120363419134907062, 120363418759047982],
-  },
-  fakeReply: {
-    provider: "whatsapp",
-  },
 });
 
 wa.on("messages", async (ctx) => {
-  console.log("ctx: ", ctx);
-  if (!ctx.citation?.isMy) return;
-
-  // wa.text('sdkjhs')
-
-  const s = await wa.profile().check({ senderId: ctx.senderId });
-  console.log("s: ", s);
+  if (ctx.text == ".zanjay") {
+    wa.reply("oii");
+  }
 });

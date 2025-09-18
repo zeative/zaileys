2d7c4fae1c25094a431b42f41c4ec86f5f3478da
zeative <zaadevofc@gmail.com>
2025-09-18 15:55:34 +0700

    delete: all files recovery

--- src/types/adapter/chats.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-import { MessagesParserType } from "../parser/messages";
-
-export const chatsGetMessage = MessagesParserType
-
-export type chatsAdapter = {
-  getMessage: (chatId: string) => Promise<z.infer<typeof chatsGetMessage> | null>;
-};

6e6210861275199aed579008a5da18944e26caf6
zeative <zaadevofc@gmail.com>
2026-05-30 07:35:03 +0700

    docs(examples): use auto-connect in quickstart

--- examples/quickstart-connect.ts
+++ examples/quickstart-connect.ts
@@ -1,9 +1,9 @@
 import { Client } from '../src/index.js'
 
-const client = new Client({})
+const client = new Client()
 
 client.on('qr', ({ qrString }) => console.log('Scan QR:', qrString))
 client.on('connect', ({ me }) => console.log('Connected as', me.id))
-client.on('disconnect', ({ reason }) => console.log('Disconnected:', reason))
-
-await client.connect()
+client.on('text', async (msg) => {
+  await client.send(msg.jid).text('Halo!')
+})

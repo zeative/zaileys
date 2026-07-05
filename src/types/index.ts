acc840722065c7991ff9111bb08afc83c1cc7695
zeative <zaadevofc@gmail.com>
2026-07-05 22:12:37 +0700

    feat(types): re-export baileys message types from zaileys

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export * from './lid-mapping.js'
 export * from './store-error.js'
 export * from '../builder/index.js'
+export type { WAMessage, WAMessageKey, WAMessageContent, AnyMessageContent } from 'baileys'
